ef066529c3c8ba94f05d6db33cdfaf1e1141373f
jack smith <testmjmj192@gmail.com>
2022-04-15 16:47:40 +0530

    create jQuery file and in this file only jQuery code.

---
+++ jQuery.js
@@ -0,0 +1,26 @@
+//this fuction is hide the element 
+$(document).ready(function(){
+    $('#hide').click(function(){
+        $('#blue').hide();
+    });
+    // this fuction is show the function
+$('#show').click(function(){
+  $('#blue').show();
+});
+// this function is fadeIn function
+$('#fadeIn').click(function(){
+  $('#fade1').fadeIn();
+  $('#fade2').fadeIn(200);
+  $('#fade3').fadeIn(300);
+});
+//this function is fadeOut function 
+$('#fadeOut').click(function(){
+  $('#fade1').fadeOut();
+  $('#fade2').fadeOut(200);
+  $('#fade3').fadeOut(300);
+});
+
+// in this function will be slide show
+
+
+});
